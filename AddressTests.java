464d93be6804548a0f5c72e0c891f03861712915
cjywish <30422987+cjywish@users.noreply.github.com>
2017-10-25 17:30:18 +0900

    local update commit

--- AddressTests.java
+++ AddressTests.java
@@ -2,6 +2,7 @@ import java.net.*;
 
 public class AddressTests {
 /* test update 1 */
+/* local update 1 */
   public static int getVersion(InetAddress ia) {
     byte[] address = ia.getAddress();
     if (address.length == 4) return 4;
