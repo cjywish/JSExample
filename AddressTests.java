a97c70ff174885e995378369fbda5c8e340c9ba1
cjywish <30422987+cjywish@users.noreply.github.com>
2017-10-25 17:57:40 +0900

    solve the conflict

--- AddressTests.java
+++ AddressTests.java
@@ -3,11 +3,11 @@ import java.net.*;
 public class AddressTests {
 /* test update 1 */
 /* local update 1 */
-<<<<<<< HEAD
+
 /* lcoal update 2 */
-=======
+
   /* remote update2 */
->>>>>>> dbef51f2b19c50b7c05c303d00f488b48861cef1
+
   public static int getVersion(InetAddress ia) {
     byte[] address = ia.getAddress();
     if (address.length == 4) return 4;
